d8bf0457333da7abd7e4c59c46c8b3c92c45fef2
Sona22890 <sonakanwar8302@gmail.com>
2024-12-09 17:25:51 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY TU ADMIN NHI HAI TU CHUTIYA HAI
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7951189538:AAEjaVf6RmrGxNd_WgK-IVmX4kO_R9eBb4A")
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("soldiersri")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("5477802914")
+
+
+
+
+  
